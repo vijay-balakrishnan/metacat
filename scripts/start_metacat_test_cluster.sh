d64dbeeb4f77dfea43d1ceb9a5db74439392c9e3
vbalakrishan <vbalakrishnan@grubhub.com>
2019-09-01 20:58:47 -0500

    adding elastic changes

--- scripts/start_metacat_test_cluster.sh
+++ scripts/start_metacat_test_cluster.sh
@@ -8,6 +8,17 @@ echo DOCKER env is $(env | grep DOCKER)
 # Usage: ./startCluster.sh docker-compose.yml
 COMPOSE_FILE=$1
 
+docker-compose --file ${COMPOSE_FILE} up -d elasticsearch  #>> build/docker_compose.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "Unable to start metacat service container"
+    exit 11
+fi
+
+docker-compose --file ${COMPOSE_FILE} up -d kibana #>> build/docker_compose.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "Unable to start metacat service container"
+    exit 11
+fi
 
 docker-compose --file ${COMPOSE_FILE} up storage-barrier #>> build/docker_compose.log 2>&1
 if [ $? -ne 0 ]; then
@@ -21,22 +32,13 @@ if [ $? -ne 0 ]; then
     exit 10
 fi
 
-docker-compose --file ${COMPOSE_FILE} up -d elasticsearch  #>> build/docker_compose.log 2>&1
-if [ $? -ne 0 ]; then
-    echo "Unable to start metacat service container"
-    exit 11
-fi
-
-docker-compose --file ${COMPOSE_FILE} up -d kibana #>> build/docker_compose.log 2>&1
-if [ $? -ne 0 ]; then
-    echo "Unable to start metacat service container"
-    exit 11
-fi
-
 docker-compose --file ${COMPOSE_FILE} up metacat-barrier #>> build/docker_compose.log 2>&1
 if [ $? -ne 0 ]; then
     echo "Unable to start metacat service container"
     exit 11
 fi
+
+
+
 
 
